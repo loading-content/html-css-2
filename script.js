1c1a6d37010ec0ee707b922d5ae19b6d802b61d5
loading-content <56720934+loading-content@users.noreply.github.com>
2023-01-17 14:22:18 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-  let link = document.createElement("link");
-  link.href = "style.css";      /**** your CSS file ****/
-  link.rel = "stylesheet";
-  link.type = "text/css";
-  frames[0].document.head.appendChild(link); /**** 0 is an index of your iframe ****/
-}
